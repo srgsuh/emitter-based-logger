69bdee4e3b0c1857d1f6d36077d6dde60d0d1dcc
srgsuh <srg.suhoverhov@gmail.com>
2025-07-16 23:08:16 +0300

    Add support for configurable log levels via `default.json`

--- logger.ts
+++ logger.ts
@@ -1,4 +1,5 @@
 import EventEmitter from "node:events";
+import config from "config";
 
 type LogLevel = "trace" | "debug" | "info" | "warn" | "critical";
 
@@ -7,11 +8,26 @@ interface LogEvent {
     message: string;
 }
 
+const LEVEL_PRIORITY: Record<string, number> = {
+    trace: 0,
+    debug: 1,
+    info: 2,
+    warn: 3,
+    critical: 4,
+}
+
 export default class Logger {
+    public static DEFAULT_LEVEL: LogLevel = "info";
+    public static CONFIG_LEVEL_KEY: string = "log_level";
+    private static ERR_PATTERN= (value: string) =>
+        `Config key "${Logger.CONFIG_LEVEL_KEY}" value "${value}" is invalid. Expected one of: ${Object.keys(LEVEL_PRIORITY).join(", ")}.`;
+
+    private priority: number = LEVEL_PRIORITY[Logger.DEFAULT_LEVEL];
     private emitter = new EventEmitter();
 
     constructor() {
         this.emitter = new EventEmitter();
+        this.priority = LEVEL_PRIORITY[this.getConfigLevel()];
     }
 
     addLevelHandler(level: LogLevel, messageHandler: (message: string) => void): void {
@@ -25,5 +41,16 @@ export default class Logger {
 
     addHandler(handler: (e: LogEvent) => void): void {
         this.emitter.on("log", handler);
+    }
+
+    private getConfigLevel(): LogLevel {
+        if (config.has(Logger.CONFIG_LEVEL_KEY)) {
+            const stringValue: string = config.get(Logger.CONFIG_LEVEL_KEY);
+            if (!(stringValue in LEVEL_PRIORITY)) {
+                throw new Error(`${Logger.ERR_PATTERN(stringValue)}`);
+            }
+            return (stringValue as LogLevel);
+        }
+        return Logger.DEFAULT_LEVEL;
     }
 }
